0ef93e2d57347491ce9165834d63e2ae9ff0d2f0
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-01-10 23:47:29 +0530

    Update TriggerAlert.java

--- src/main/java/TypewiseAlert/TriggerAlert.java
+++ src/main/java/TypewiseAlert/TriggerAlert.java
@@ -8,18 +8,14 @@ public class TriggerAlert {
 	    }
 	    public static void sendToEmail(BreachTypeEnum breachType) {
 	      String recepient = "a.b@c.com";
-	      switch(breachType) {
-	        case TOO_LOW:
-	          System.out.printf("To: %s\n", recepient);
-	          System.out.println("Hi, the temperature is too low\n");
-	          break;
-	        case TOO_HIGH:
-	          System.out.printf("To: %s\n", recepient);
-	          System.out.println("Hi, the temperature is too high\n");
-	          break;
-	        case NORMAL:
-	          break;
+	      if(breachType != BreachTypeEnum.NORMAL)
+	      {
+	    	  printConsoleBasedOnBreachType(breachType,recepient);
 	      }
 	    }
-
+	    public static void printConsoleBasedOnBreachType(BreachTypeEnum breachType,String recepient)
+	    {
+	    	System.out.printf("To: %s\n", recepient);
+	        System.out.println("Hi, the temperature is too low\n");
+	    }
 }
