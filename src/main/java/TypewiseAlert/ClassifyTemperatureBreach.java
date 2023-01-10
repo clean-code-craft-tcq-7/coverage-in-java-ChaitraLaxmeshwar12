6c6071568bfd2fca91e62c4a38b2028db2ff3fc4
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-01-11 00:04:25 +0530

    Update ClassifyTemperatureBreach.java

--- src/main/java/TypewiseAlert/ClassifyTemperatureBreach.java
+++ src/main/java/TypewiseAlert/ClassifyTemperatureBreach.java
@@ -3,7 +3,7 @@ package TypewiseAlert;
 public class ClassifyTemperatureBreach {
 	
 	 public static BreachTypeEnum classifyTemperatureBreach(CoolingTypeEnum coolingType, double temperatureInC) {
-		      return InferBreach.inferBreach(temperatureInC, lowerLimit, upperLimit);
+		      return InferBreach.inferBreach(temperatureInC, coolingType.lowerLimit, coolingType.upperLimit);
 		    }
 
 }
