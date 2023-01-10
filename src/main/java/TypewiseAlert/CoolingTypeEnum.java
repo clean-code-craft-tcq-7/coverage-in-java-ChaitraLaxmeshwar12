6bfdf070cf2932e8f83514f0b8b6d6ab0e4730fe
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-01-10 23:39:30 +0530

    Update CoolingTypeEnum.java

--- src/main/java/TypewiseAlert/CoolingTypeEnum.java
+++ src/main/java/TypewiseAlert/CoolingTypeEnum.java
@@ -2,8 +2,16 @@ package TypewiseAlert;
 
 public enum CoolingTypeEnum {
 	
-	 PASSIVE_COOLING,
-     HI_ACTIVE_COOLING,
-     MED_ACTIVE_COOLING
+     PASSIVE_COOLING(0,35),
+     HI_ACTIVE_COOLING(0,45),
+     MED_ACTIVE_COOLING(0,40)
+	     
+     Integer lowerLimit = null;
+     Integer upperLimit = null;
+
+	public ECoolingType(Integer lowerLimit, Integer upperLimit) {
+		this.lowerLimit = lowerLimit;
+		this.upperLimit = upperLimit;
+	} 
 
 }
